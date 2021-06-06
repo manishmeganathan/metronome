6d1de9bbcdd9eb830c380f1e0119bf90781a9e8e
Manish Meganathan <757manish@gmail.com>
2021-06-06 19:42:33 +0530

    Update core lib
    - Updated the main.dart and home.dart files to include the animated clock

--- clockapp/lib/home.dart
+++ clockapp/lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:clockapp/clockview.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,6 +11,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: Color(0xFF2D2F41),
+        child: ClockView(),
+      )
+    );
   }
 }
