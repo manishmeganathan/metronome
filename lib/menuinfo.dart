f8c75ef55d6efb3a5bacb7b920bbf6775fc159cc
Manish Meganathan <757manish@gmail.com>
2021-06-12 22:46:08 +0530

    Add new menu info files
    - Added files enums.dart, data.dart and menuinfo.dart that contains the menu navigation tools

---
+++ lib/menuinfo.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rooster/enums.dart';
+
+class MenuInfo extends ChangeNotifier {
+  MenuType menuType;
+  String? title;
+  String? imagesrc;
+
+  MenuInfo(this.menuType, {this.title, this.imagesrc});
+
+  updateMenu(MenuInfo menuinfo) {
+    this.menuType = menuinfo.menuType;
+    this.title = menuinfo.title;
+    this.imagesrc = menuinfo.imagesrc;
+
+    notifyListeners();
+  }
+}
