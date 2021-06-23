82ba534adfe221c97601154fce3eab7c78d7891b
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-23 18:42:59 +0530

    Add app theme
    - Added a file theme.dart in a directory /data
    - Added app theme colors (just the basics)

---
+++ lib/data/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color white = Colors.white;
+  static Color grey = Color(0xFF908E8E);
+  static Color black = Color(0xFF0E121B);
+  static Color blue = Color(0xFF00D9F6);
+}
